ccfa3a52b48c8d11c3bdacb2e6040d302d439c01
Xylometazol <xylometazol@hotmail.com>
2017-06-06 21:21:13 +0200

    Fixed get resource button
    automatically generate resource table

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,44 @@
+/// <reference path="jquery.d.ts"/>
 var Resource = (function () {
-    function Resource(tableCellID) {
-        this.tableCellID = tableCellID;
+    // Function that's run when objects are created to give initial values 
+    function Resource(resourceName) {
+        this.tableCell = null;
+        this.name = resourceName;
         this.amount = 0;
     }
+    // Function to set how much of a resource one has and convert it to string
     Resource.prototype.setAmount = function (value) {
-        this.amount = 0;
-        document.getElementById(this.tableCellID).innerHTML = value.toString();
+        var table = $("#resourceTable");
+        if (this.tableCell == null) {
+            table.append($("<tr>\n                            <td>" + this.name + "</td>\n                            <td id=\"amount" + this.name + "\">0</td>\n                            </tr>"));
+            this.tableCell = $("#amount" + this.name);
+        }
+        this.amount = value;
+        this.tableCell.text(value.toString());
     };
+    // Used to make "amount" usable outside of "Resource" class
     Resource.prototype.getAmount = function () {
         return this.amount;
     };
     return Resource;
 }());
-var wood = new Resource("amountWood");
-window.onload = function () {
-    wood.setAmount();
-};
+var wood, stone, copper, tin, iron, coal, steel;
+// Function being called by index.html for button to add one wood
 function addWood() {
     wood.setAmount(wood.getAmount() + 1);
 }
+function addStone() {
+    stone.setAmount(stone.getAmount() + 1);
+}
+$(function () {
+    var resources = [
+        wood = new Resource("Wood"),
+        stone = new Resource("Stone"),
+        copper = new Resource("Copper"),
+        tin = new Resource("Tin"),
+        iron = new Resource("Iron"),
+        coal = new Resource("Coal"),
+        steel = new Resource("Steel")
+    ];
+});
 //# sourceMappingURL=scripts.js.map
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,28 +1,63 @@
+/// <reference path="jquery.d.ts"/>
+
 class Resource {
-    private tableCellID: string;
+    // Private creates a variable in this scope (Resource)
+    private tableCell: JQuery;
     private amount: number;
+    private name: string;
 
-    constructor(tableCellID:string){
-        this.tableCellID = tableCellID;
+    // Function that's run when objects are created to give initial values 
+    constructor(resourceName:string){
+        this.tableCell = null;
+        this.name = resourceName;
         this.amount = 0;
     }
 
+    // Function to set how much of a resource one has and convert it to string
     setAmount(value:number):void{
-        this.amount = 0;
-        document.getElementById(this.tableCellID).innerHTML = value.toString();
+        var table: JQuery = $("#resourceTable");
+
+        if(this.tableCell == null){
+            table.append($(`<tr>
+                            <td>${this.name}</td>
+                            <td id="amount${this.name}">0</td>
+                            </tr>`));
+
+            this.tableCell = $("#amount"+this.name);
+        }
+    
+        this.amount = value;
+        this.tableCell.text(value.toString());
+
     }
 
+    // Used to make "amount" usable outside of "Resource" class
     getAmount(): number {
         return this.amount;
     }
 }
 
-let wood = new Resource("amountWood");
+var wood,stone,copper,tin,iron,coal,steel;
 
-window.onload = function() {
-    wood.setAmount(1);
-}
-
+// Function being called by index.html for button to add one wood
 function addWood(): void {
     wood.setAmount(wood.getAmount() + 1);
+}function addStone(): void {
+    stone.setAmount(stone.getAmount() + 1);
 }
+
+$(function() {
+
+    var resources: Resource[] = [
+        wood = new Resource("Wood"),
+        stone = new Resource("Stone"),
+        copper = new Resource("Copper"),
+        tin = new Resource("Tin"),
+        iron = new Resource("Iron"),
+        coal = new Resource("Coal"),
+        steel = new Resource("Steel")
+    ];
+});
+
+
+
